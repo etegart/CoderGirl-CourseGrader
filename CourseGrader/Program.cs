69f8a7c207d375508baf736b4c5c7be6d5471ba1
etegart <etegart@gmail.com>
2019-03-11 23:41:14 -0500

    Passes all, but 1 test

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -11,6 +11,73 @@ namespace CourseGrader
             // It should then return a string of "pass" or "fail" depending on these two conditions:
             // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
             // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
+
+
+            int[] testScores = new int[5];
+            testScores[0] = 48;
+            testScores[1] = 55;
+            testScores[2] = 90;
+            testScores[3] = 95;
+            testScores[4] = 95;
+
+
+            GradeTestScores(testScores);
+            Console.WriteLine(GradeTestScores(testScores));
+            Console.ReadLine();
+
+        }
+
+
+        public static string GradeTestScores(int[] inputArray)
+        {
+
+
+            int sum = 0;
+            string pass = "pass";
+            string fail = "fail";
+            
+            
+            foreach (var score in inputArray)
+            {
+                sum = sum + score;
+            }
+
+            float average = sum / inputArray.Length;
+
+            foreach (var number in inputArray)
+            {
+                if (average >= 70)
+                {
+                    if (number < 50)
+                    {
+                        return fail;
+                    }
+                }
+            }
+            
+            if (average >= 70)
+            {
+                return pass;
+            }
+
+            else if (average < 70)
+            {
+                return fail;
+            }
+
+            else
+            {
+                return fail;
+            }
+
+                   
+                    
+            
+                    
+                 
+
         }
     }
 }
+
+
